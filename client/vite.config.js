f42f4d2b8707342ed9744b88c0b988cbac4fe26b
shingo29831 <shingo29831@gmail.com>
2026-08-19 00:10:24 +0900

    fix(vite): add proxy configuration for socket.io in dev server

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/socket.io/': {
+        target: 'http://server:5000', // docker-compose内のサーバーコンテナ名:ポート
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })
